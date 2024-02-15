079365756aafa24d87863cbbd7647e6732be9a7f
sanket <sanket.arak@gmail.com>
2024-02-15 23:57:46 +0530

    create app and tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-4 text-xl font-bold text-blue-700">
+      Hello, Lets get started with creating your own NETFLIX APP
     </div>
   );
 }
