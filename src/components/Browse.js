dd9e1831587d91b10527d3eef06cce7bc22bef3d
sanket <sanket.arak@gmail.com>
2024-02-20 00:05:21 +0530

    register TMDB and create api key, call the api using documentation

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
+import { APIT_OPTIONS, NOW_PLAYING_MOVIES } from "../utils/constants";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(NOW_PLAYING_MOVIES, APIT_OPTIONS);
+    const json = await data.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,15 @@ export const USER_LOGO =
   "https://occ-0-4344-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABbkvkH9DYGUFVfqsK2bX1qFGEd2mNbSbJfUv5x3AdSAFCNXHgHF9IcjprV-SHKg3wEyaQIKWNOJECyv8hiUT6QiXfAYuiSc.png?r=fab";
 
 export const PHOTO_URL = "https://example.com/jane-q-user/profile.jpg";
+
+export const APIT_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkN2I0NGUzNGVhMzNlNGM2NTc3Yjk5Y2VlYjQyNGFkYiIsInN1YiI6IjY1ZDM5OTdjMzg3NjUxMDE4NjMyYzgzNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IjOStMshwt9FCWAK-3WFS4szXw31NcBEnSELJtB5gz0",
+  },
+};
+
+export const NOW_PLAYING_MOVIES =
+  "https://api.themoviedb.org/3/movie/now_playing?page=1";
